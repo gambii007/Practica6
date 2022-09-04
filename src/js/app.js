1f3ef9e9f7743336c81d542855e6304a21d996d3
FernandoGR <gamboa_029@hotmail.com>
2022-09-04 04:11:26 -0500

    P6V5

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,39 @@ document.addEventListener('DOMContentLoaded', function () {
 
 function iniciarApp() {
     crearGaleria();
+    navegacionFija();
+    crearGaleria();
+    scrollNav();
+}
+
+function navegacionFija() {
+    const barra = document.querySelector('.header');
+    const sobreFestival = document.querySelector('.sobre-festival');
+    const body = document.querySelector('body');
+
+    window.addEventListener('scroll', function() {
+
+        if (sobreFestival.getBoundingClientRect().bottom < 10) { //Cuando el elemento toque la parte de arriba
+            barra.classList.add('fijo');
+            body.classList.add('body-scroll');
+        }else{
+            barra.classList.remove('fijo');
+            body.classList.remove('body-scroll');
+        }
+    });
+}
+
+function scrollNav() {
+    const enlances = document.querySelectorAll('.navegacion-principal a');
+    enlances.forEach( enlace => {
+        enlace.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            seccionScroll = e.target.attributes.href.value;
+            const seccion = document.querySelector(seccionScroll);
+            seccion.scrollIntoView({ behavior: "smooth"});
+        })
+    })
 }
 
 function crearGaleria() {
